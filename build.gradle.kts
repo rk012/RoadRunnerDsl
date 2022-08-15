6b6a32ea5121bb99be13f1f07f5480405bc4e4ee
Rishi Kumar <rishi.kumar.2837@gmail.com>
2022-08-15 18:00:37 -0400

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.7.10"
+}
+
+group = "io.github.rk012"
+version = "0.1-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+
+    maven("https://maven.brott.dev/")
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+
+    implementation("com.acmerobotics.roadrunner:core:0.5.5")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
